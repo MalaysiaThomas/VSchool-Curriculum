199025f896aaef9ae964434596812a7f19e5f964
Malaysia Thomas <malaysiathomas16@gmail.com>
2024-08-07 20:23:57 -0400

    completed

---
+++ assignments/exercises/613-dj-js-event-listener-practice/app.js
@@ -0,0 +1,47 @@
+let square = document.getElementById("shape");
+
+//Square default background color set to orange in styles.css
+
+//Turns square blue on hover
+square.addEventListener("mousemove", function() {
+    square.style.backgroundColor = "blue"
+})
+
+square.addEventListener("mouseout", function() {
+    square.style.backgroundColor = ""
+})
+
+//Turns square red when mouse is held down over shape
+square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "red"
+})
+
+//Turns square yellow when mouse is lifted over shape
+square.addEventListener("mouseup", function() {
+    square.style.backgroundColor = "yellow"
+})
+
+//Turns square green when double-clicked
+square.addEventListener("dblclick", function() {
+    square.style.backgroundColor = "green"
+})
+
+//Colors square based on key pressed
+document.addEventListener("keydown", function(e){
+    let key = e.key
+    if (key === "b" && "B") {
+        square.style.backgroundColor = "blue";
+    } else if (key === "r" && "R") {
+        square.style.backgroundColor = "red";
+    } else if (key === "y" && "Y") {
+        square.style.backgroundColor = "yellow";
+    } else if (key === "g" && "G") {
+        square.style.backgroundColor = "green";
+    } else if (key === "o" && "O") {
+        square.style.backgroundColor = "orange";
+    } else if (e.getModifierState("CapsLock")) {
+        alert("Turn off caps lock")
+    } else {
+        alert("Invalid Selection. Try One of These Instead:\nType 'o' for orange\nType 'b' for blue\nType 'r' for red\nType 'y' for yellow\nType 'g' for green")
+    }
+})
