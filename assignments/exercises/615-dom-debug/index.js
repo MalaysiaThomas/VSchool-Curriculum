51e8d8d2b9f9b69e90490538dcfb9c5484a5599c
Malaysia Thomas <malaysiathomas16@gmail.com>
2024-08-08 16:40:08 -0400

    submission 1

--- assignments/exercises/615-dom-debug/index.js
+++ assignments/exercises/615-dom-debug/index.js
@@ -1,32 +1,48 @@
-const colors = ["red", "blue", "green"]
+//Background color options for listed items
+const colors = ["white", "red", "blue", "green"]
 
-document.getElementById("add").addEventListener("onclick", function(e){
-    const subItem = createSubItem(e)
-    document.getElementById("list").appendChild(subItem)
+//Get input and div from index.html
+const input = document.getElementById("input")
+const list = document.getElementById("list")
+
+//Create ul element; add as child to div id="list"
+const subItemList = document.createElement("ul")
+list.appendChild(subItemList)
+
+
+//Add items to list
+document.getElementById("add").addEventListener("click", function(){
+    const userInput = input.value
+    const subItem = document.createElement("li")    
+    subItem.textContent = userInput
+    subItemList.appendChild(subItem)
+    createDropDown()
 })
 
+
+//Create dropdown to select color
 function createDropDown(){
-    const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    const dropDown = document.createElement("select")    
+    let listedItems = document.getElementsByTagName("li")
+    //Get color options from 'colors' array
+    for (let i = 0; i < colors.length; i++) {
+        const option = document.createElement("option") 
         option.innerHTML = colors[i]
         option.value = colors[i]
         dropDown.append(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    //Adds dropdown to each item
+    for (let i = 0; i < listedItems.length; i++) {
+        listedItems[i].append(dropDown)
+    }
+    //Changes background color based on option selected
+    dropDown.addEventListener("change", function(e){
+        e.target.parentElement.style.backgroundColor = e.target.value
+        //Changes text color to white for increased readability if background is colored
+        if (e.target.value != "white") {
+            e.target.parentElement.style.color = "white"
+        } else {
+            e.target.parentElement.style.color = "black"
+        }
     })
-    return dropDown
 }
-
-function createSubItem(e){
-    const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
-    const dropDown = createDropDown()
-    subItem.appendChild(dropDown)
-    subItem.setAttribute("class", "subItem")
-    return subItem
-}
-
-
