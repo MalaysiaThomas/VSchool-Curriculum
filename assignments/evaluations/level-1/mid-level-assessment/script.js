5f34c4c2bf6569bc19268bea92c4a16cd273fc0e
Malaysia Thomas <malaysiathomas16@gmail.com>
2024-08-13 17:11:44 -0400

    submitted

--- assignments/evaluations/level-1/mid-level-assessment/script.js
+++ assignments/evaluations/level-1/mid-level-assessment/script.js
@@ -1,18 +1,17 @@
-//Create function that takes an element's id and changes its text content
+// Create function that takes an element's id and changes its text content
 let changeTextContent = (elementId, newText) => {
-    let selectedElement = document.getElementById(elementId)
-    selectedElement.textContent = newText
-}
+  let selectedElement = document.getElementById(elementId);
+  selectedElement.textContent = newText;
+};
 
 // Get the button with id "changeButton"
-let changeButton = document.getElementById("changeButton")
+let changeButton = document.getElementById("changeButton");
 
 changeButton.addEventListener("click", () => {
-    // Get the first p tag in the document
-    let paragraph = document.getElementsByTagName("p")[0]
-    // Set p tag id to "paragraph"
-    paragraph.setAttribute('id', 'paragraph')
-    // Call changeTextContent function
-    changeTextContent('paragraph', 'And that button just made me change!')
-})
-
+  // Get the first p tag in the document
+  let paragraph = document.getElementsByTagName("p")[0];
+  // Set p tag id to "paragraph"
+  paragraph.setAttribute("id", "paragraph");
+  // Call changeTextContent function
+  changeTextContent("paragraph", "And that button just made me change!");
+});
